0b02e2484e033875bff53dd540fe96db53d62157
Gurrdhillon <78093727+Gurrdhillon@users.noreply.github.com>
2021-11-14 16:14:27 -0800

    Added Swagger

--- Project/calhfaapi/Startup.cs
+++ Project/calhfaapi/Startup.cs
@@ -27,8 +27,13 @@ namespace calhfaapi
   // This method gets called by the runtime. Use this method to add services to the container.
   public void ConfigureServices(IServiceCollection services)
   {
+      
 
    services.AddControllers();
+    services.AddSwaggerGen(c =>
+    {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "calhfaapi", Version = "v1" });
+    });
 
    //add the name of the class build in Services
    services.AddTransient<CalHFAService>();
@@ -40,8 +45,8 @@ namespace calhfaapi
    if (env.IsDevelopment())
    {
     app.UseDeveloperExceptionPage();
-    // app.UseSwagger();
-    // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "calhfaapi v1"));
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "calhfaapi v1"));
    }
 
    app.UseHttpsRedirection();
